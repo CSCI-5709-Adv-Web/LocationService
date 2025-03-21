f360094a457a4cf16d803228f2bac401b1469e0f
JASH-PATEL-6566 <pateljash1511.com>
2025-03-21 01:12:28 -0300

    change url

--- src/express-app.ts
+++ src/express-app.ts
@@ -31,8 +31,8 @@ app.use((req, res, next) => {
 });
 
 // API routes
-app.use("/location/docs", docsRoutes);
-app.use("/location/location", locationRoutes);
+app.use("/api/docs", docsRoutes);
+app.use("/api/location", locationRoutes);
 
 // Add a healthcheck endpoint
 app.get("/health", (req, res) => {
